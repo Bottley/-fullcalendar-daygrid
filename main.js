e0db34b40af57b8c0bb5986d04570fccd1743b7c
Bottley <brad@innovativethinking.co.za>
2020-06-01 04:35:47 +0200

    Add Unique ID to Table Cells

--- main.js
+++ main.js
@@ -708,7 +708,7 @@ Docs & License: https://fullcalendar.io/
         var isDateValid = core.rangeContainsMarker(dateProfile.activeRange, date); // TODO: called too frequently. cache somehow.
         var classes = core.getDayClasses(date, dateProfile, context);
         classes.unshift('fc-day', theme.getClass('widgetContent'));
-        return '<td class="' + classes.join(' ') + '"' +
+        return '<td' + ' id="' + dateEnv.formatIso(date, { omitTime: true }) + '" class="' + classes.join(' ') + '"' +
             (isDateValid ?
                 ' data-date="' + dateEnv.formatIso(date, { omitTime: true }) + '"' :
                 '') +
